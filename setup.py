3344be1e0189710f189b2a5ead1779de29d057bf
kakalan123 <kakalan123@botimize.io>
2017-01-16 15:10:58 +0800

    pyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'botimize-sdk-py',
+  packages = ['botimize-sdk-py'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Botimize python SDK',
+  author = 'kakalan123',
+  author_email = 'kakalan123@botimize.io',
+  url = 'https://github.com/botimize/botimize-sdk-python', # use the URL to the github repo
+  download_url = 'https://github.com/botimize/botimize-sdk-python/tarball/1.0', # I'll explain this in a second
+  keywords = ['botimize', 'python', 'sdk'], # arbitrary keywords
+  classifiers = [],
+)
