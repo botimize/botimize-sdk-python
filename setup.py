cd1d789a72dd1f099c93207bfeac3b5cdb5f9243
kakalan123 <a612051@gmail.com>
2017-06-07 17:27:26 +0800

    update to 1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'botimize',
   packages = ['botimize'],
-  version = '1.1',
+  version = '1.2',
   description = 'Botimize python SDK',
   author = 'botimize',
   author_email = 'dev@botimize.io',
   url = 'https://github.com/botimize/botimize-sdk-python',
-  download_url = 'https://github.com/botimize/botimize-sdk-python/tarball/1.1',
+  download_url = 'https://github.com/botimize/botimize-sdk-python/tarball/1.2',
   keywords = ['botimize', 'python', 'sdk', 'chatbot', 'analytics'],
   classifiers = [],
 )
